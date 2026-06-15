0c1e8024c30ef330712121e70226a1f960b50067
Jishant tanwar <princetanwar995@gmail.com>
2026-06-16 02:07:33 +0530

    connections made in mysql

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,10 +74,17 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'LittleLemon',  
+        'USER': 'root',  
+        'PASSWORD': 'Jishant@2006',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    } 
 }
 
 
